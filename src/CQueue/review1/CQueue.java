a43e3fa8caeb79678b15eed30393ba2281173851
RuanZzzz <734162396@qq.com>
2022-03-01 11:02:51 +0800

    完善两个栈实现列表的测试

--- src/CQueue/review1/CQueue.java
+++ src/CQueue/review1/CQueue.java
@@ -2,6 +2,19 @@ package CQueue.review1;
 
 import java.util.LinkedList;
 
+/**
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：
+ * ["CQueue","appendTail","deleteHead","deleteHead"]
+ * [[],[3],[],[]]
+ * 输出：[null,null,3,-1]
+ *。
+ */
 public class CQueue {
     LinkedList<Integer> A,B;
 
@@ -31,4 +44,12 @@ public class CQueue {
         return B.removeLast();
     }
 
+    public static void main(String[] args) {
+        CQueue cQueue = new CQueue();
+        cQueue.appendTail(3);
+        cQueue.deleteHead();
+        cQueue.deleteHead();
+        //System.out.println(cQueue.deleteHead());
+        //System.out.println(cQueue.deleteHead());
+    }
 }
